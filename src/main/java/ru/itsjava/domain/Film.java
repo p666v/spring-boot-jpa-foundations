bd134e7d745b3fc33ea7208d5f8531c8b882ec19
Pavel Konovalov <konovalovpv.81@gmail.com>
2022-10-31 13:43:41 +0300

    Spring Boot JPA. Виды связей между таблицами

--- src/main/java/ru/itsjava/domain/Film.java
+++ src/main/java/ru/itsjava/domain/Film.java
@@ -1,13 +1,21 @@
 package ru.itsjava.domain;
 
 import javax.persistence.*;
+import java.util.List;
 
-@Entity
-@Table(name = "films")
+@Entity(name = "films")
 public class Film {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String title;
+    @ManyToOne(targetEntity = Genre.class, cascade = CascadeType.ALL)
+    @JoinColumn(name = "genre_id")
+    private Genre genre;
+
+    @OneToMany(targetEntity = Place.class, fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "film_id")
+    private List<Place> places;
+
 
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,10 @@
-INSERT INTO films (title) VALUES ('Doom');
-INSERT INTO films (title) VALUES ('Harry Potter');
+INSERT INTO genre (name) VALUES ('fantasy');
+INSERT INTO genre (name) VALUES ('fantastic');
+
+
+INSERT INTO films (title, genre_id) VALUES ('Doom', 2);
+INSERT INTO films (title, genre_id) VALUES ('Harry Potter', 1);
+
+
+INSERT INTO place (name, film_id) VALUES ('London', 2);
+INSERT INTO place (name, film_id) VALUES ('Mars', 1);
